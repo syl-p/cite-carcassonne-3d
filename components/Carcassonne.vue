d0e755fab0268cdd2f19d2b65d823ef962383c73
syl-p <sylvain@atnos.com>
2024-08-07 09:06:23 +0200

    fix: change terrain tif

--- components/Carcassonne.vue
+++ components/Carcassonne.vue
@@ -1,21 +1,24 @@
 <template>
+  <MapTerrain />
   <TresGroup v-if="data">
     <Natural :features="naturalFeatures" v-if="naturalFeatures" />
     <Urban :features="buildingsFeatures" v-if="buildingsFeatures" />
-    <MapWater :features="waterFeatures" v-if="waterFeatures"/>
+    <MapWater :features="waterFeatures" v-if="waterFeatures" />
   </TresGroup>
-  <MapTerrain />
 </template>
 
 <script setup lang="ts">
 import Natural from "~/components/Map/Natural.vue";
 import Urban from "./Map/Urban.vue";
+// BBOX DEFINITION
+
+// RECUPERATION DATA
 const { data } = await useFetch("/api/osm_data");
 
+// COMPUTE SOME FEATURES
 const naturalFeatures = computed(() => {
   return data.value && data.value.natural ? data.value.natural.features : [];
 });
-
 
 const waterFeatures = computed(() => {
   return data.value && data.value.water ? data.value.water.features : [];
--- components/Map/Terrain.vue
+++ components/Map/Terrain.vue
@@ -1,39 +1,47 @@
 <template>
-    <TresMesh :geometry="geometry" receive-shadow cast-shadow>
-        <TresMeshStandardMaterial color="gray" wireframe/>
-    </TresMesh>
+  <TresMesh ref="meshRef" :geometry="geometry">
+    <TresMeshStandardMaterial color="gray" wireframe="" />
+  </TresMesh>
 </template>
 
 <script setup lang="ts">
-    import * as THREE from "three"
-    import * as GeoTIFF from "geotiff";
-    const rawTiff = await GeoTIFF.fromUrl("./terrain.tif")
-    const tiffImage = await rawTiff.getImage()
+import * as THREE from "three";
+import * as GeoTIFF from "geotiff";
+const rawTiff = await GeoTIFF.fromUrl("./terrain-2.tif");
+const tiffImage = await rawTiff.getImage();
+const meshRef = ref();
 
-    const start = [2.1673611111111111,43.1381944444444514]
-    const end = [2.4331944444444442,43.3237500000000040]
+const start = [2.353936, 43.19904];
+const end = [2.374069, 43.213273];
 
-    let leftBottom = GpsRelativePosition(start, CENTER)
-    let rightTop = GpsRelativePosition(end, CENTER)
-    let x = Math.abs(leftBottom[0]-rightTop[0])
-    let y = Math.abs(leftBottom[1]-rightTop[1])
+let leftBottom = GpsRelativePosition(end, CENTER);
+let rightTop = GpsRelativePosition(start, CENTER);
+let x = Math.abs(leftBottom[0] - rightTop[0]);
+let y = Math.abs(leftBottom[1] - rightTop[1]);
 
-    const data = await tiffImage.readRasters(
-                {width: Math.floor(x), height: Math.floor(y),
-                  resampleMethod: 'bilinear', interleave: true})
+const data = await tiffImage.readRasters({
+  width: Math.floor(x),
+  height: Math.floor(y),
+  resampleMethod: "bilinear",
+  interleave: true,
+});
 
-    const geometry = new THREE.PlaneGeometry(x, y, x-1 , y-1)
-    for (let i = 0; i < data.length; i++) {
-        const el = data[i];
-        geometry.getAttribute('position').setZ(i, - (el as number/30))
-        geometry.getAttribute('position').needsUpdate = true
-    }
+let minHeight = Infinity;
+const geometry = new THREE.PlaneGeometry(x, y, x - 1, y - 1);
+for (let i = 0; i < data.length; i++) {
+  const el = data[i];
+  const height = (el as number) / 80;
+  geometry.getAttribute("position").setZ(i, height);
 
+  if (height < minHeight) minHeight = height;
+}
 
-    onMounted(() => {
-      geometry.computeBoundingBox()
-      geometry.rotateX(Math.PI/2)
-      geometry.rotateZ(Math.PI)
-      geometry.rotateY(- Math.PI/2)
-    })
+onMounted(() => {
+  geometry.getAttribute("position").needsUpdate = true;
+  geometry.computeBoundingBox();
+  geometry.rotateX(Math.PI / 2);
+  geometry.rotateY(Math.PI / 2);
+  geometry.rotateZ(Math.PI);
+  geometry.translate(0, -minHeight, 0); // Center the terrain vertically
+});
 </script>

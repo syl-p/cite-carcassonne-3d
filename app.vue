dbe9c45e3cd41a1b801aee380789ffbb00ab8bc5
syl-p <sylvain@atnos.com>
2024-09-04 13:24:48 +0200

    feat: remove useless layout

--- app.vue
+++ app.vue
@@ -10,44 +10,38 @@
       <Experience />
     </TresCanvas>
 
-    <NuxtLayout>
-      <header class="p-8 fixed flex justify-between top-0 left-0 w-full z-50">
-        <div>
-          <NuxtLink to="/" class="flex space-x-3">
-            <div class="w-10 h-10 bg-white"></div>
-            <div class="uppercase font-spectral">
-              Cité<br />
-              de <span class="font-bold">Carcassonne</span>
-            </div>
-          </NuxtLink>
-        </div>
-        <div>
-          <ul class="flex space-x-3">
-            <li>Accueil</li>
-            <li>A propos</li>
-            <li>Contacter</li>
-          </ul>
-        </div>
-      </header>
+    <header class="p-8 fixed flex justify-between top-0 left-0 w-full z-50">
+      <div>
+        <NuxtLink to="/" class="flex space-x-3">
+          <div class="w-10 h-10 bg-white"></div>
+          <div class="uppercase font-spectral">
+            Cité<br />
+            de <span class="font-bold">Carcassonne</span>
+          </div>
+        </NuxtLink>
+      </div>
+      <div>
+        <ul class="flex space-x-3">
+          <li>Accueil</li>
+          <li>A propos</li>
+          <li>Contacter</li>
+        </ul>
+      </div>
+    </header>
 
-      <UiNavigation :pages="data" v-if="page.title == ''" />
+    <UiNavigation :pages="data" v-if="page.title == ''" />
 
-      <NuxtPage />
-    </NuxtLayout>
+    <NuxtPage />
   </main>
 </template>
 
 <script setup lang="ts">
-import {
-  BasicShadowMap,
-  LinearSRGBColorSpace,
-  NoToneMapping,
-  ReinhardToneMapping,
-  SRGBColorSpace,
-} from "three";
+import { BasicShadowMap, LinearSRGBColorSpace, NoToneMapping } from "three";
+
 const { data } = await useAsyncData("parts", () =>
   queryContent("parts").find()
 );
+
 const { page } = useContent();
 
 const gl = {
--- components/Experience.vue
+++ components/Experience.vue
@@ -1,11 +1,10 @@
 <template>
   <TresPerspectiveCamera />
   <TresAmbientLight :intensity="1" />
-  <TresDirectionalLight cast-shadow :position="[-2, 7, 4]" :intensity="1.5" />
+  <TresDirectionalLight cast-shadow :position="[-2, 10, 4]" :intensity="1.5" />
   <Suspense>
-    <Carcassonne :points="props.points" />
+    <Carcassonne />
   </Suspense>
-
   <EffectComposer>
     <Vignette :darkness="1.3" :offset="0" />
   </EffectComposer>
@@ -16,8 +15,4 @@ import * as THREE from "three";
 const { camera } = useTresContext();
 const initialOffset = new THREE.Vector3(1, 3, 4);
 usePageLocalisation(initialOffset, camera);
-
-const props = defineProps({
-  points: Array<any>,
-});
 </script>
